2032d1841155a345d5d54e422b0084c8bee57324
Vanessa Sotero <soterovanessa@gmail.com>
2024-12-03 11:35:18 -0300

    estrutura do projeto e login failed

---
+++ test/specs/login.e2e.js
@@ -0,0 +1,13 @@
+import { expect, browser, $ } from "@wdio/globals";
+
+describe("Login", () => {
+  it("Login sucess", async () => {
+    await $(
+      '//android.view.ViewGroup[@content-desc="open menu"]/android.widget.ImageView'
+    ).click(); //Clica no menu 3 linhas
+    await $("//*[@text= 'Log In']").click(); //Escolhe a opção de login
+    await $("~Username input field").setValue("username");
+    await $("~Password input field").setValue("senha");
+    await $("~Login button").click();
+  });
+});
