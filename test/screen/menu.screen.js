497c19e811ef21a257f5a1f42ef572d90d4e0f7b
Vanessa Sotero <soterovanessa@gmail.com>
2024-12-03 14:31:36 -0300

    create: create menu screen

---
+++ test/screen/menu.screen.js
@@ -0,0 +1,17 @@
+class MenuScreen {
+  constructor() {
+    (this.buttonLeftMenu =
+      '//android.view.ViewGroup[@content-desc="open menu"]/android.widget.ImageView'),
+      (this.buttonLeftLogin = "//*[@text= 'Log In']"),
+      (this.fieldUsername = "~Username input field"),
+      (this.fieldPassword = "~Password input field"),
+      (this.buttonLogin = "~Login button");
+  }
+
+  async accessLoginScreen() {
+    await $(this.buttonLeftMenu).click();
+    await $(this.buttonLeftLogin).click();
+  }
+}
+
+export default new MenuScreen();
--- test/specs/login.e2e.js
+++ test/specs/login.e2e.js
@@ -1,11 +1,10 @@
 import { expect, browser, $ } from "@wdio/globals";
+import MenuScreen from "../screen/menu.screen";
 
 describe("Login", () => {
   it("Login sucess", async () => {
-    await $(
-      '//android.view.ViewGroup[@content-desc="open menu"]/android.widget.ImageView'
-    ).click(); //Clica no menu 3 linhas
-    await $("//*[@text= 'Log In']").click(); //Escolhe a opção de login
+    await MenuScreen.accessLoginScreen();
+
     await $("~Username input field").setValue("username");
     await $("~Password input field").setValue("senha");
     await $("~Login button").click();
